8f5207c5761f7b78e407dae502e0759f55b57125
Hello_Dev0ps! <hello_dev0ps@protonmail.com>
2025-06-03 19:39:21 +0200

    SEO

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -7,7 +7,7 @@ function Accueil() {
         <MeteoWidget/>
         <h1>Bienvenue dans notre cabinet infirmier de Valence d'Albigeois !</h1>
           <p>
-            Nous sommes à votre écoute pour tous vos soins à domicile ou au cabinet, avec professionnalisme et bienveillance, 7J/7.
+            Nous sommes à votre écoute pour tous vos soins infirmiers à domicile ou au cabinet, avec professionnalisme et bienveillance, prise de rendez-vous 7J/7.
           </p>
         <img src={cabinetImage} alt="Cabinet infirmier" style={{ maxWidth: '75%', borderRadius: '12px', marginTop: '1rem' }} />
       </div>
